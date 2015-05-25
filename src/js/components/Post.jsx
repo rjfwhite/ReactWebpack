900f1184990fb5d49c7fab083d3eb78726d6c5cf
Robert Whitehead <stuff@rjfw.co.uk>
2015-05-25 12:40:33 +0100

    formatting changes & new file

--- src/js/components/Post.jsx
+++ src/js/components/Post.jsx
@@ -2,6 +2,7 @@
 'use strict'
 var React = require('react')
 module.exports = React.createClass({
+    displayName: 'HelloReact',
     render: function(){
         return <div>Hello React</div>
     }
--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,12 @@ module.exports = {
     module: {
         loaders: [
             {
-                //tell webpack to use jsx-loader for all *.jsx files
                 test: /\.jsx$/,
                 loader: 'jsx-loader?insertPragma=React.DOM&harmony'
             }
         ]
     },
     externals: {
-        //don't bundle the 'react' npm package with our bundle.js
-        //but get it from a global 'React' variable
         'react': 'React'
     },
     resolve: {
