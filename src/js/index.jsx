60b333d9a630190d0e621f93a6bdec5168d7b4c4
Robert Whitehead <stuff@rjfw.co.uk>
2015-05-25 12:29:22 +0100

    Initial commit

---
+++ src/js/index.jsx
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+'use strict'
+var React = require('react')
+var Hello = require('./components/Hello')
+React.renderComponent(<Hello />, document.getElementById('content'))
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./src/js/index.jsx",
+    output: {
+        path: __dirname,
+        filename: "bundle.js",
+        publicPath: 'http://localhost:8090/assets'
+    },
+    module: {
+        loaders: [
+            {
+                //tell webpack to use jsx-loader for all *.jsx files
+                test: /\.jsx$/,
+                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            }
+        ]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
